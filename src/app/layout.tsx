5a962a3184f5720d523d5cdd2325c9274f79c116
ssaymontorres <saymontorres48@gmail.com>
2026-02-25 14:59:20 -0300

    fix(layout): add suppressHydrationWarning to handle external class injection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         <meta
           name="viewport"
